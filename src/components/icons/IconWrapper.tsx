2a50b295853107d94002fa4d903f3202533be593
castlefurnitureworks@gmail.com <castlefurnitureworks@gmail.com>
2021-07-05 17:09:35 -0700

    moved function to avoid circular dependency issue

---
+++ src/components/icons/IconWrapper.tsx
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import { useRomaine } from "romaine";
+
+export interface IconProps
+  extends React.DetailedHTMLProps<
+    React.HTMLAttributes<HTMLDivElement>,
+    HTMLDivElement
+  > {
+  selected?: boolean | string;
+  tooltip?: string;
+}
+
+export const IconWrapper = ({
+  selected,
+  children,
+  tooltip,
+  ...props
+}: IconProps) => {
+  const {
+    romaine: { mode },
+  } = useRomaine();
+  const [hover, sethover] = useState<boolean>(false);
+  return (
+    <abbr title={tooltip}>
+      <div
+        onMouseEnter={() => sethover(true)}
+        onMouseLeave={() => sethover(false)}
+        style={{
+          border:
+            selected === true || selected === mode || hover
+              ? "thin solid black"
+              : "",
+          borderRadius: "4px",
+          backgroundColor: hover
+            ? "#888"
+            : selected === true || selected === mode
+            ? "#555"
+            : "#fff0",
+          display: "grid",
+          placeItems: "center",
+          height: "40px",
+        }}
+        {...props}
+      >
+        {children}
+      </div>
+    </abbr>
+  );
+};
--- src/components/icons/index.tsx
+++ src/components/icons/index.tsx
@@ -1,32 +1,5 @@
-import React, { useState } from "react";
-
-export interface IconProps
-  extends React.DetailedHTMLProps<
-    React.HTMLAttributes<HTMLDivElement>,
-    HTMLDivElement
-  > {
-  selected: boolean;
-}
-
-export const IconWrapper = ({ selected, children, ...props }: IconProps) => {
-  const [hover, sethover] = useState(selected);
-  return (
-    <div
-      onMouseEnter={() => sethover(true)}
-      onMouseLeave={() => sethover(false)}
-      style={{
-        border: selected || hover ? "thin solid black" : "",
-        borderRadius: "4px",
-        backgroundColor: hover ? "#888" : selected ? "#555" : "#fff0",
-        display: "grid",
-        placeItems: "center",
-        height: "40px",
-      }}
-      {...props}
-    >
-      {children}
-    </div>
-  );
-};
-
+export * from "./IconWrapper";
 export * from "./CropperIcon";
+export * from "./PerspectiveIcon";
+export * from "./RotateLeft";
+export * from "./RotateRight";
