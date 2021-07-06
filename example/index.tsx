dcd7baad12ad6511ae3567cc9fd699c15ff71380
castlefurnitureworks@gmail.com <castlefurnitureworks@gmail.com>
2021-07-05 17:08:20 -0700

    added a way to fetch the image as blob

--- example/index.tsx
+++ example/index.tsx
@@ -1,24 +1,35 @@
 import React, { useRef, useState } from "react";
 import { Canvas, Romaine, RomaineRef, useRomaine } from "romaine";
-import { PerspectiveIcon } from "../src/components/icons/PerspectiveIcon";
 import {
   CropperIcon,
   FolderSelection,
+  PerspectiveIcon,
+  RotateLeft,
+  RotateRight,
 } from "../src/romaine_components.development";
-export const RomaineExample = () => {
+interface RomaineExampleProps {
+  setBlob?: React.Dispatch<React.SetStateAction<Blob | null>>;
+}
+export const RomaineExample = ({ setBlob }: RomaineExampleProps) => {
   return (
     <Romaine>
-      <ChildComponent />
+      <ChildComponent setBlob={setBlob} />
     </Romaine>
   );
 };
-
-export const ChildComponent = () => {
+/**
+ *
+ * @todo 1) move the get blob button into its own file
+ */
+export const ChildComponent = ({ setBlob }: RomaineExampleProps) => {
   const RomaineRef = useRef<RomaineRef>(null);
-  const { loaded } = useRomaine();
-  const [state, setstate] = useState<File | string | null>(null);
+  const { loaded, setMode } = useRomaine();
+  const [state, setstate] = useState<File | string | null>(
+    "https://source.unsplash.com/random"
+  );
+
   return (
-    <div>
+    <div style={{ marginTop: "4em" }}>
       {loaded && (
         <Canvas
           ref={RomaineRef}
@@ -29,6 +40,7 @@ export const ChildComponent = () => {
           onDragStop={() => {}}
           pointSize={5}
           lineWidth={1}
+          wrapperProps={{ style: { border: "thin solid black" } }}
         >
           <FolderSelection
             image={state}
@@ -38,6 +50,43 @@ export const ChildComponent = () => {
               {state ? "Choose a Different File" : "Choose or Drag a File Here"}
             </span>
           </FolderSelection>
+          <button
+            style={{
+              position: "absolute",
+              right: 0,
+              bottom: "3ch",
+              zIndex: 400,
+              border: "thin solid black",
+              borderRadius: 0,
+              fontSize: "16px",
+              background: "white",
+              width: "240px",
+            }}
+            onClick={async () => {
+              setMode && setMode(null);
+              // need to let mode actually get set to null
+              // React 18 useTransition would be nice here...
+              // but for backwards compatability doing this...
+              setTimeout(async () => {
+                if (setBlob && RomaineRef.current?.getBlob) {
+                  const newBlob =
+                    (await RomaineRef.current?.getBlob({
+                      type: "image/jpeg",
+                    })) || null;
+                  setBlob(newBlob);
+                } else {
+                  if (RomaineRef.current?.getBlob)
+                    console.log(
+                      "Image as Blob: ",
+                      await RomaineRef.current?.getBlob({ type: "image/jpeg" })
+                    );
+                  console.warn("You must give the example setBlob as an input");
+                }
+              }, 0);
+            }}
+          >
+            Export Image
+          </button>
           <div
             style={{
               position: "absolute",
@@ -50,6 +99,8 @@ export const ChildComponent = () => {
             <div
               style={{ display: "grid", gridTemplateColumns: "repeat(6, 1fr)" }}
             >
+              <RotateLeft />
+              <RotateRight />
               <CropperIcon />
               <PerspectiveIcon />
             </div>
