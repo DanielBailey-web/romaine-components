c6ebb3eb4681c7e343a3febc1aad4cce83e6d898
castlefurnitureworks@gmail.com <castlefurnitureworks@gmail.com>
2021-07-05 17:35:49 -0700

    fixed circular dependency between icons

--- src/components/icons/PerspectiveIcon.tsx
+++ src/components/icons/PerspectiveIcon.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { IconWrapper } from ".";
+import { IconWrapper } from "./IconWrapper";
 import { useRomaine } from "romaine";
 
 interface Props
--- src/components/icons/RotateLeft.tsx
+++ src/components/icons/RotateLeft.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { IconWrapper } from ".";
+import { IconWrapper } from "./IconWrapper";
 import { useRomaine } from "romaine";
 
 interface Props
